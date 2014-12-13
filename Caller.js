ee4d91c1a10654c91857140fe1bcc2514255d300
rnairs <r.nairs@gmail.com>
2014-12-14 00:13:02 +0400

    It's WebService Comm

---
+++ Caller.js
@@ -0,0 +1,12 @@
+﻿function test() {
+    $.ajax({ url: "http://155.155.155.60/SB/JSONP-EndPoint.asmx/Sum",
+    data: { x: JSON.stringify("Now i am getting jsop string") },
+        dataType: "jsonp",
+        success: function(json) {
+            alert(json.d);
+        },
+        error: function() {
+            alert("Hit error fn!");
+        }
+    });
+}
